6a81d6cebc407842cc59ed7fa2edae387bac8903
mogesTesema <moges2421@gmail.com>
2025-09-20 12:02:32 +0300

    try execpt error handling in python

---
+++ app.py
@@ -0,0 +1,31 @@
+class Car:
+    def __init__(self,make,model):
+        self.make = make
+        self.model = model
+    def __repr__(self):
+        return f"<Car {self.make} {self.model}>"
+
+
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+    def __len__(self):
+        return len(self.cars)
+    def add_car(self,car):
+        if not isinstance(car,Car):
+            raise ValueError(f"Tried to add a {car.__class__.__name__} to the garage, but you can only add '{Car.__name__}' object.")
+        self.cars.append(car)
+        # raise NotImplementedError("We can't add cars to the garage yet")
+ford = Garage()
+car = Car("ford","Fiesta")
+ford.add_car(car)
+print(len(ford))
+try:
+    ford.add_car("hello")
+except TypeError as e:
+    print("some thing went wrong: ",e)
+except ValueError as e:
+    print("value error hitted:",e)
+finally:
+    print(f"The garage now has {len(ford)}")
--- error-handling.py
+++ error-handling.py
@@ -1,2 +1,23 @@
-student = [2,232,2,2,2]
-print(student[""])
+# class AstuError(TypeError):
+#     def __repr__(self,message,code):
+#         super().__init__(message)
+#         self.code = code
+#         self.message = message
+#     def __perp__(self):
+#         return f'<user defined {self.___class__.__name__} Error>'
+#     def add(self, a,b):
+#        raise AstuError(f"{self.__class__.__name__} unexpected final exam Error. universty name and actual universty clashing. unexpected sleeping.",self.code)
+# raise AstuError("this is testing error",5500)
+
+class MyCustomError(TypeError):
+    """
+    this is custom error to handle 500 internal server error issue.
+    """
+    def __init__(self,message,code):
+        super().__init__(f"Error code {code}: {message}")
+        self.code = code
+        self.message = message
+error = MyCustomError("OUCH! An error happend.", 500)
+print(error.__doc__)
+raise error
+

bdc949985edd926d5bb2826fc2107b8b29cbae59
mogesTesema <moges2421@gmail.com>
2025-09-20 09:14:58 +0300

    oop in python and decorator in a class

---
+++ error-handling.py
@@ -0,0 +1,2 @@
+student = [2,232,2,2,2]
+print(student[""])
--- python-class.py
+++ python-class.py
@@ -9,13 +9,13 @@ create chatbot class with the following attribute and method:
 7. history()
     can view n most recent chats, and delete any chat if necessary
 8. recent()
-
 """
+
 import heapq
+from datetime import datetime
 class ChatBot:
     def __init__(self,name):
         self.name = name
-        self.time = 0
         self.chats = []
 
     def __len__(self):
@@ -31,8 +31,7 @@ class ChatBot:
         return f"chatbot system that interact with user that has {len(self.chats)} chats till now."
 
     def chat(self,chat):
-        curr = self.time
-        self.time += -1
+        curr = datetime.utcnow()
         self.chats.append((curr,chat))
 
     def history(self):
@@ -72,5 +71,97 @@ print("\n"*4)
 for chat in user:
     print(chat)
 print(len(user))
-
+"""
+inheritance is the beauty of OOP what encourage DRY principle(Don't repeat yourself)
+"""
+class ProfChatBot(ChatBot):
+    def __init__(self,name,title,salary=0):
+        super().__init__(name)
+        self.title = title
+        self.salary = salary
+    @property
+    def annual_salary(self):
+        return f"Hello {self.title} {self.name} your anuall salary is {self.salary*12}$."
     
+
+prof = ProfChatBot("Alemayehu","Dr.",120)
+print(prof.annual_salary)
+prof.chat("why my salary is not increasing?")
+prof.chat(f"Hello! I am Dr. Alemayehu. please increase my salary, I can't servive life with this {prof.salary}$ salary.")
+prof.chat("if you don't increase salary, I will leave your company.")
+print(prof.history())
+print("\n",prof.recent())
+print("\n",prof.annual_salary)
+
+"""
+Decoratory in python:
+@property:- used to change simple class method to just property of the class if this method don'e perform action rather than calculation 
+@classmethod:- used to pass class as argument for class method that used to get info about the class itself
+@staticmethod:- used to conviy info about the class but don't use neither class properties nor class method
+#instancemethod:- used to pass instance(object) of the class to access propertiy and method of the instance for its usecase(every method in class is by default such type of method )
+"""
+
+class Foo:
+    @classmethod # this decorator used to pass class as argument 
+    def hi(cls): # take the objects class as argument
+        return print(cls.__name__) 
+    @staticmethod
+    def hello():
+        print("ell, I don't take parameters.")
+
+foo_object = Foo()
+foo_object.hi()
+foo_object.hello()
+class FixedFloat:
+    def __init__(self,amount):
+        self.amount = amount
+    def __repr__(self):
+        return f"<FixedFloat {self.amount:.2f}"
+    @classmethod
+    def from_sum(cls,value_one, value_two):
+        return cls(value_one + value_two)
+
+number = FixedFloat(18.5762)
+print(number)
+new_number = FixedFloat.from_sum(10.244,242.2424)
+print(new_number)
+  
+class Dollar(FixedFloat):
+    def __init__(self,amount):
+        super().__init__(amount)
+        self.symbol ='$'
+    def __repr__(self):
+        return f"<Dollar {self.symbol}{self.amount:.2f}"
+money = Dollar(18.2424)
+print(money)
+dollar = Dollar.from_sum(4243.3434,34.3434)
+print(dollar)
+print(dir(heapq))
+print(datetime.now())
+import json
+print(dir(json))
+x = {
+  1: "John",
+  2: 30,
+  3: "New York"
+}
+y = json.dumps(x)
+print(y)
+x = json.loads(y)
+print(x)
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
+import re
+print(dir(re))

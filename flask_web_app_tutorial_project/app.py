77d9efa8e2d6b76b40690019872a00864b774fce
mogesTesema <moges2421@gmail.com>
2025-10-30 21:15:24 +0300

    jinja2 inheritance,Post and get method basic, URL redirection

--- flask_web_app_tutorial_project/app.py
+++ flask_web_app_tutorial_project/app.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 app = Flask(__name__)
 
 posts = {
-    0:{
+    0:{ 'post_id':0,
         'title':'hello,world',
         'content':'This is my first blog post!'
     }
@@ -11,14 +11,30 @@ posts = {
 
 @app.route("/")
 def home():
-    return "<h1>hello,Flask world</h1>"
+    discribe = "this is awesome Flask web app for education purpose"
+    return render_template("home.html",description=discribe)
 
-@app.route("/post/<int:post_id>") # /post/0 <int:post_id> is place holder for post id that are coming from frontend.and
+@app.route("/post/<int:post_id>") # /post/0 <int:post_id> is place holder for post id that are coming from frontend.
 def posted(post_id):
     post = posts.get(post_id)
     if not post:
-        return render_template("404.html",message=f'A post with id{post_id} was not found.')
+        return render_template("404.html",message=f'A post with id {post_id} was not found.')
     return render_template("post.html",post=post)
+@app.route("/post/form")
+
+def form():
+    return render_template("create.jinja2")
+
+#127.0.0.1:5000/post/create?title=something&content=something_else
+@app.route("/post/create", methods=["POST"])
+def create():
+    # title = request.args.get("title")
+    # content = request.args.get("content")
+    title = request.form.get("title")
+    content = request.form.get("content")
+    post_id = len(posts)
+    posts[post_id] = {"post_id":post_id, "title":title, "content":content}
+    return redirect(url_for("posted",post_id=post_id))
 
 if __name__ == "__main__":
     app.run(debug=True )

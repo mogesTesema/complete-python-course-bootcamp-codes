44566574d81e2488eb774981351d9334143951af
mogesTesema <moges2421@gmail.com>
2025-11-07 19:51:59 +0300

    unittest in python simple divide function test using unittest module TestCase class

---
+++ testing/functions.py
@@ -0,0 +1,6 @@
+from typing import Union
+
+def divide(dividend:Union[int,float],divsor:Union[int,float]):
+    return dividend/divsor
+
+# print("division results:\n",divide(10,2.3))
---
+++ testing/test_functions.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from functions import divide
+
+
+class TestFunctions(TestCase):
+    def test_divide_result(self):
+        dividend = 15
+        divisor = 3
+        expected_results = 5.0
+        self.assertAlmostEqual(divide(dividend,divisor),expected_results,delta=0.0001)
+    def test_divide_negative(self):
+        dividend = 15
+        divisor = -3
+        expected_result = -5.0
+        self.assertAlmostEqual(divide(dividend,divisor),expected_result,delta=0.001)
+    def test_divide_dividend_zero(self):
+        dividend = 0
+        divisor = 5
+        expected_result = 0
+        self.assertAlmostEqual(divide(dividend,divisor),expected_result,delta=0.1)
